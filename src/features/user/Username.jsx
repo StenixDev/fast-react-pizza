6e27030a4be7fcb724ef75966f5bfe3f9f78ebc2
StenixDev <stenix.dev@gmail.com>
2025-10-24 08:48:35 +0800

    Modeling the "User" State With Redux Toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Stenix</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 export default Username;
